df26ac755c68e559ccdc4f2897bf5e4128038fc3
Kr0wZ <67439924+Kr0wZ@users.noreply.github.com>
2022-11-17 15:58:13 +0100

    2.0 version
    Introduce the light mode using the protonmail API

--- neutrosint.py
+++ neutrosint.py
@@ -5,6 +5,7 @@ from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
+from selenium.common.exceptions import UnexpectedAlertPresentException, TimeoutException
 
 import time
 import os
@@ -13,9 +14,10 @@ import requests
 from datetime import datetime
 from colorama import Fore, Style
 
-#TODO
-
-#Add exceptions -> verify if file exists (when loading emails) + proxy server if no connection available
+#Check if the number of emails to verify is greater than 100. In the case where the user uses the light mode, print a message saying that all email addresses won't be able to be tested. Redirect the user
+#to the credentials mode.
+#If the user uses the credentials mode then check if more than 100 email addresses. If yes then automatically remove from the field from 'To' once we reach the limit and do it again until there
+#are no more email addresses to verify
 
 class NeutrOSINT():
 	def __init__(self):
@@ -25,19 +27,21 @@ class NeutrOSINT():
 		self.emails = []
 		self.time = 16
 		self.output_file = None
-		self.proxy = None
+		self.proxy = {}
+		self.light = False
+		self.max_retries = 0
 
 	def banner(self):
 		print("""
 
-		 ██████   █████                      █████                 ███████     █████████  █████ ██████   █████ ███████████
-		░░██████ ░░███                      ░░███                ███░░░░░███  ███░░░░░███░░███ ░░██████ ░░███ ░█░░░███░░░█
-		 ░███░███ ░███   ██████  █████ ████ ███████   ████████  ███     ░░███░███    ░░░  ░███  ░███░███ ░███ ░   ░███  ░ 
-		 ░███░░███░███  ███░░███░░███ ░███ ░░░███░   ░░███░░███░███      ░███░░█████████  ░███  ░███░░███░███     ░███    
-		 ░███ ░░██████ ░███████  ░███ ░███   ░███     ░███ ░░░ ░███      ░███ ░░░░░░░░███ ░███  ░███ ░░██████     ░███    
-		 ░███  ░░█████ ░███░░░   ░███ ░███   ░███ ███ ░███     ░░███     ███  ███    ░███ ░███  ░███  ░░█████     ░███    
-		 █████  ░░█████░░██████  ░░████████  ░░█████  █████     ░░░███████░  ░░█████████  █████ █████  ░░█████    █████   
-		░░░░░    ░░░░░  ░░░░░░    ░░░░░░░░    ░░░░░  ░░░░░        ░░░░░░░     ░░░░░░░░░  ░░░░░ ░░░░░    ░░░░░    ░░░░░    
+		  ██████   █████                      █████                 ███████     █████████  █████ ██████   █████ ███████████                 ████████ 
+		 ░░██████ ░░███                      ░░███                ███░░░░░███  ███░░░░░███░░███ ░░██████ ░░███ ░█░░░███░░░█                ███░░░░███
+		  ░███░███ ░███   ██████  █████ ████ ███████   ████████  ███     ░░███░███    ░░░  ░███  ░███░███ ░███ ░   ░███  ░     █████ █████░░░    ░███
+		  ░███░░███░███  ███░░███░░███ ░███ ░░░███░   ░░███░░███░███      ░███░░█████████  ░███  ░███░░███░███     ░███       ░░███ ░░███    ███████ 
+		  ░███ ░░██████ ░███████  ░███ ░███   ░███     ░███ ░░░ ░███      ░███ ░░░░░░░░███ ░███  ░███ ░░██████     ░███        ░███  ░███   ███░░░░  
+		  ░███  ░░█████ ░███░░░   ░███ ░███   ░███ ███ ░███     ░░███     ███  ███    ░███ ░███  ░███  ░░█████     ░███        ░░███ ███   ███      █
+		  █████  ░░█████░░██████  ░░████████  ░░█████  █████     ░░░███████░  ░░█████████  █████ █████  ░░█████    █████        ░░█████   ░██████████
+		 ░░░░░    ░░░░░  ░░░░░░    ░░░░░░░░    ░░░░░  ░░░░░        ░░░░░░░     ░░░░░░░░░  ░░░░░ ░░░░░    ░░░░░    ░░░░░          ░░░░░    ░░░░░░░░░░ 
 
 			""")
 
@@ -46,7 +50,7 @@ class NeutrOSINT():
 		chrome_options.add_argument("--headless")
 		chrome_options.add_argument('--no-sandbox')
 
-		if(self.proxy != None):
+		if(len(self.proxy) != 0):
 			chrome_options.add_argument('--proxy-server=%s' % self.proxy)
 
 		service = Service(ChromeDriverManager().install())
@@ -74,6 +78,9 @@ class NeutrOSINT():
 	def set_proxy(self, proxy):
 		self.proxy = proxy
 
+	def set_light_mode(self, light):
+		self.light = light
+
 	def load_emails(self, file):
 		try:
 			handle = open(file, 'r')
@@ -82,6 +89,12 @@ class NeutrOSINT():
 				self.emails.append(line)
 
 			handle.close()
+
+			#Tell the user the API limit will be exceeded
+			if(len(self.emails) < 100):
+				if(self.light):
+					print(Fore.YELLOW + "[?] Warning! There are more than 100 email addresses to check. The API's limit is 100 requests. All the email addresses won't be tested. You can either use the credentials mode (--username and --password) or use a proxy (--proxy)")
+
 		except:
 			print(Fore.RED + "[-] Unable to load emails")
 			exit()
@@ -120,12 +133,20 @@ class NeutrOSINT():
 			print(Fore.RED + "[-] Error when connecting to ProtonMail...")
 			exit()
 
+	def clear_element(self, method, element_path):
+		try:
+			wait_element = WebDriverWait(self.driver, self.time).until(EC.presence_of_element_located((method, element_path)))
+			element = self.driver.find_element(method, element_path)
+			element.clear()
+		except:
+			print(Fore.RED + "[-] Unable to clear the element")
+			exit()
 
 	def new_email(self):
 		try:
 			print(Fore.YELLOW + "[?] Accessing 'New email' to check email addresses...")
 			#Retrieve the "New email" button
-			element = WebDriverWait(self.driver, self.time).until(EC.element_to_be_clickable((By.XPATH, '//button[@class="button button-large button-solid-norm w100 no-mobile"]')))
+			element = WebDriverWait(self.driver, 30).until(EC.element_to_be_clickable((By.XPATH, '//button[@class="button button-large button-solid-norm w100 no-mobile"]')))
 			new_email_element = self.driver.find_element(By.XPATH, '//button[@class="button button-large button-solid-norm w100 no-mobile"]')
 			new_email_element.click()
 
@@ -135,18 +156,45 @@ class NeutrOSINT():
 			element = WebDriverWait(self.driver, self.time).until(EC.presence_of_element_located((By.XPATH, '/html/body/div[1]/div[4]/div/div/div/div/div/div[2]/div/div/div/div/div/div/input')))
 			to_email_element = self.driver.find_element(By.XPATH, '/html/body/div[1]/div[4]/div/div/div/div/div/div[2]/div/div/div/div/div/div/input')
 
+			print(Fore.YELLOW + "[?] Checking email addresses...\n" + Style.RESET_ALL)
+
+			#Create a copy of emails to avoid the modifications
+			tmp_emails_array = self.emails[:]
+
+			while(len(tmp_emails_array) > 100):
+				#Check the first 100 emails
+				emails_to_check_str = ','.join(tmp_emails_array[:100])
+				#Convert list to string + add "," at the end (for the last occurence to be updated)
+				emails_to_check_str += ','
+
+				#Write all emails in the "To" field and wait until protonmail detects if they exist or not
+				to_email_element.send_keys(emails_to_check_str)
+
+				self.check_emails(tmp_emails_array[:100])
+
+				#Remove the first 100 email addresses
+				tmp_emails_array = tmp_emails_array[100:]
+				#Clear the 'To' field
+				to_email_element.clear()
+
+			#If we are here it means there are less than 100 email addresses
+			emails_to_check_str = ','.join(tmp_emails_array)
 			#Convert list to string + add "," at the end (for the last occurence to be updated)
-			emails_to_check_str = ','.join(self.emails)
 			emails_to_check_str += ','
+			
 
 			#Write all emails in the "To" field and wait until protonmail detects if they exist or not
 			to_email_element.send_keys(emails_to_check_str)
+			self.check_emails(tmp_emails_array)
 
-			time.sleep(self.time)
-			print(Fore.GREEN + "[+] Ready to check emails\n")
-		except:
-			print(Fore.RED + "[-] Unable to access new email to check email addresses")
-			exit()
+		except TimeoutException:
+			if(self.max_retries == 1):
+				print(Fore.RED + "[-] Too many retries. Try to launch the script again")
+				exit()
+			self.max_retries = self.max_retries + 1
+			print(Fore.RED + "[-] Unable to access new email to check email addresses. Trying again...")
+			self.new_email()
+			
 
 
 	def extract_timestamp(self, email):
@@ -161,14 +209,16 @@ class NeutrOSINT():
 			return creation_date
 
 		except AttributeError:
-			print(Fore.RED + "[-] Error! Impossible to retrieve the creation date")
+			#print(Fore.RED + "[-] Error! Impossible to retrieve the creation date. Maybe API restriction...")
+			return None
 		
 
-	def check_emails(self):
+	#Pass emails as argument to use for printing (pass tmp_emails_array)
+	def check_emails(self, emails):
 		try:
-			print(Fore.YELLOW + "[?] Checking email addresses...\n" + Style.RESET_ALL)
+			#print(Fore.YELLOW + "[?] Checking email addresses...\n" + Style.RESET_ALL)
 			#Retrieve the emails we inserted in the input field
-			element = WebDriverWait(self.driver, self.time).until(EC.presence_of_element_located((By.XPATH, '/html/body/div[1]/div[4]/div/div/div/div/div/div[2]/div/div/div/div/div/div')))
+			element = WebDriverWait(self.driver, 5).until(EC.presence_of_element_located((By.XPATH, '/html/body/div[1]/div[4]/div/div/div/div/div/div[2]/div/div/div/div/div/div')))
 			elements_to_loop = self.driver.find_elements(By.XPATH, '/html/body/div[1]/div[4]/div/div/div/div/div/div[2]/div/div/div/div/div/div')
 
 			#Remove the last div (which is always null)
@@ -182,26 +232,68 @@ class NeutrOSINT():
 				class_str = item.get_attribute('class')
 				if("invalid" in class_str):
 					if(self.output_file != None):
-						self.write_to_file("Invalid email: " + self.emails[count] + "\n")
-					print(Fore.RED + "[-] Invalid email: " + self.emails[count] + Style.RESET_ALL)
+						self.write_to_file("Invalid email: " + emails[count] + "\n")
+					print(Fore.RED + "[-] Invalid email: " + Style.RESET_ALL + emails[count])
 				else:
-					creation_date = self.extract_timestamp(self.emails[count])
+					creation_date = self.extract_timestamp(emails[count])
 					if(self.output_file != None):
-						self.write_to_file("Valid email: " + self.emails[count] + " - Creation date: " + str(creation_date) + Style.RESET_ALL + "\n")
-					print(Fore.GREEN + "[+] Valid email: " + self.emails[count] + " - Creation date: " + str(creation_date))
+						self.write_to_file("Valid email: " + emails[count] + " - Creation date: " + str(creation_date) + "\n")
+					print(Fore.GREEN + "[+] Valid email: " + Style.RESET_ALL + emails[count] + " - Creation date: " + str(creation_date))
 
 				count = count + 1
 		except UnexpectedAlertPresentException:
 			print(Fore.RED + "[-] Unable to check emails addreses...")
 			exit()
+		except IndexError:
+			pass
 
 	def close(self):
 		self.driver.close()
 
+	def request_api(self):
+		for email in self.emails:
+			try:
+				request = requests.get("https://account.proton.me/api/users/available", 
+					headers={
+						"x-pm-appversion":"web-account@5.0.11.11",
+						"x-pm-locale":"en_US"
+					},
+					params={
+						"Name":email,
+						"ParseDomain":"1"
+					},
+					proxies=self.proxy)
+
+				#Return code 429 = API limite exceeded
+				if(request.status_code == 409):
+					creation_date = self.extract_timestamp(email)
+					if(self.output_file != None):
+						self.write_to_file("Valid email: " + email + " - Creation date: " + str(creation_date) + "\n")
+					print(Fore.GREEN + "[+] Valid email: " + Style.RESET_ALL + email + " - Creation date: " + str(creation_date))
+
+				elif(request.status_code == 429):
+					print(Fore.RED + "[-] API requests limit exceeded... Try with the credentials mode (--username and --password) or use a proxy (--proxy)")
+				else:
+					if(self.output_file != None):
+						self.write_to_file("Invalid email: " + email + Style.RESET_ALL + "\n")
+					print(Fore.RED + "[-] Invalid email: " + Style.RESET_ALL + email)
+
+			except:
+				print("Error when requesting the API")
+
+
 	def run(self):
-		self.banner()
-		self.setup()
-		self.login()
-		self.new_email()
-		self.check_emails()
-		self.close()
+		try:
+			self.banner()
+
+			#Check if we are running in light mode
+			if(self.light):
+				self.request_api()
+			else:
+				self.setup()
+				self.login()
+				self.new_email()
+				self.close()
+		except KeyboardInterrupt:
+			print(Fore.YELLOW + "[?] Exiting..." + Style.RESET_ALL)
+			return
--- options.py
+++ options.py
@@ -15,8 +15,9 @@ class Options:
 		#Group where options cannot be called together
 		exclusive_group_actions = self.parser.add_mutually_exclusive_group()
 
-		self.parser.add_argument("-u", "--username", metavar="USERNAME", required=True, type=str, help="Username to connect to your ProtonMail account")
-		self.parser.add_argument("-p", "--password", metavar="PASSWORD", required=True, type=str, help="Password to connect to your ProtonMail account")
+		self.parser.add_argument("-l", "--light", action="store_true", help="Light mode in which we call the API instead of connecting with credentials. Useful when few emails to check")
+		self.parser.add_argument("-u", "--username", metavar="USERNAME", type=str, help="Username to connect to your ProtonMail account")
+		self.parser.add_argument("-p", "--password", metavar="PASSWORD", type=str, help="Password to connect to your ProtonMail account")
 
 		exclusive_group_actions.add_argument("-f", "--file", metavar="FILE", type=str, help="Specify containing list of emails to check")
 		exclusive_group_actions.add_argument("-e", "--email", metavar="EMAIL", type=str, help="Check existence of this email")
@@ -27,6 +28,13 @@ class Options:
 
 		self.args = self.parser.parse_args()
 
+		#If none of these arguments is used
+		if(not "light" in self.args and not ("username" in self.args and "password" in self.args)):
+			self.parser.error("Error! You must specify either the light mode -l/--light or the credentials (-u/--username and -p/--password)")
+
+		if("light" in self.args and ("username" in self.args or "password" in self.args)):
+			self.parser.error("Error! You must specify either the light mode -l/--light or the credentials (-u/--username and -p/--password)")
+
 		if(not "file" in self.args and not "email" in self.args):
 			self.parser.error("Error! You must specify one of the two options: -f/--file or -e/--email")
 
@@ -36,8 +44,11 @@ class Options:
 
 	def run(self):
 
-		self.neutrosint.set_username(self.args.username)
-		self.neutrosint.set_password(self.args.password)
+		if("light" in self.args):
+			self.neutrosint.set_light_mode(self.args.light)
+		else:
+			self.neutrosint.set_username(self.args.username)
+			self.neutrosint.set_password(self.args.password)
 
 		if("proxy" in self.args):
 			self.neutrosint.set_proxy(self.args.proxy)
